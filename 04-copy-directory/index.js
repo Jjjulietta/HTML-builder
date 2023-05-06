a14603974e8b3e4578d5cfe952539e4e076ce1ca
Jjjulietta <jjjuliettai@gmail.com>
2023-05-06 22:25:36 +0300

    feat: complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout} = process;
 
+function copyDir() {
+  fs.mkdir(path.join(__dirname, 'files-copy'), {recursive: true},  err =>{
+    if(err) {throw err;}
+  });
+  fs.readdir(path.join(__dirname, 'files-copy'), (error, files) =>{
+    if(error) {throw error;}
+    files.forEach(element => fs.unlink(path.join(__dirname, 'files-copy', element), err => {
+      if(err) throw err;
+    }));
+  });
+  fs.readdir(path.join(__dirname, 'files'), (error, files) => {
+    if(error) {throw error;}
+    files.forEach(element => fs.copyFile(path.join(__dirname, 'files', element), path.join(__dirname, 'files-copy', element), (err) => {
+      if(err) throw err;
+    }));
+  });
+  stdout.write(path.join(__dirname, 'files-copy'));
+}
+copyDir();
